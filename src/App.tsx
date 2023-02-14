ad20acab24e9adccd3af866069cb878a4a2cc612
ZeeshanMustfai <zeeshanmustfai22@gmail.com>
2023-02-14 23:26:51 +0500

    make editor doe

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { marked } from "marked";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  const [text, setText] = useState("");
+  const handleChange = (event: any) => {
+    setText(event.target.value);
+  };
+  const renderHtml = () => {
+    const result = marked(text);
+    return { __html: result };
+  };
+  return (
+    <div className="App">
+      <textarea
+        id="editor"
+        onChange={handleChange}
+        value={text}
+        placeholder="enter text here"
+        style={{ width: 400, height: 300 }}
+      />
+
+      <div id="preview" dangerouslySetInnerHTML={renderHtml()} />
+    </div>
+  );
 }
 
 export default App;
